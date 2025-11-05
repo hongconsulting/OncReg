669076bdf136210038e58b9657bb8e37159d8413
Wei Hong <wei.hong@hongconsulting.ai>
2025-11-06 08:14:31 +1100

    v0.6.0

--- R/OncReg.line.R
+++ R/OncReg.line.R
@@ -2,14 +2,14 @@
 #'
 #' For each patient, identifies the first line of treatment whose start date
 #' occurs at least a specified number of days after any earlier stop date.
-#' @param start.dates Numeric matrix of treatment start dates, with one row per 
+#' @param start.dates Numeric matrix of treatment start dates, with one row per
 #' patient and one column per line of treatment.
-#' @param stop.dates Numeric matrix of treatment stop dates, with one row per 
+#' @param stop.dates Numeric matrix of treatment stop dates, with one row per
 #' patient and one column per line of treatment.
-#' @param min.days Minimum number of days between a stop date and a start date 
+#' @param min.days Minimum number of days between a stop date and a start date
 #' required to define a treatment restart. Default = `182.62125`.
-#' @return A numeric matrix with two columns: the first column indicating the 
-#' line of restarted treatment and the second column indicating the number of 
+#' @return A numeric matrix with two columns: the first column indicating the
+#' line of restarted treatment and the second column indicating the number of
 #' days since the previous stop date.
 #' @export
 OR.line.restart <- function(start.dates, stop.dates, min.days = 182.62125) {
@@ -40,17 +40,16 @@ OR.line.restart <- function(start.dates, stop.dates, min.days = 182.62125) {
 #'
 #' For each line of treatment, truncates the recorded stop date so that it does
 #' not exceed a specified number of days after its start date.
-#' @param start.dates Numeric matrix of treatment start dates, with one row per 
+#' @param start.dates Numeric matrix of treatment start dates, with one row per
 #' patient and one column per line of treatment.
-#' @param stop.dates Numeric matrix of treatment stop dates, with one row per 
+#' @param stop.dates Numeric matrix of treatment stop dates, with one row per
 #' patient and one column per line of treatment.
-#' @param max.days Maximum number of days allowed between each start and stop 
+#' @param max.days Maximum number of days allowed between each start and stop
 #' date. Default = `168`.
 #' @details
 #' An example use case is with oxaliplatin-containing chemotherapy regimens such
-#' as FOLFOX where the oxaliplatin component is usually limited to no more than 
-#' 24 weeks and extended dates reflect maintenance therapy with 5-fluorouracil
-#' only.
+#' as FOLFOX where the oxaliplatin component is usually limited to no more than
+#' 24 weeks and time beyond this represents treatment with 5-fluorouracil only.
 #' @return A matrix of truncated stop dates.
 #' @export
 OR.line.truncate <- function(start.dates, stop.dates, max.days = 168) {
