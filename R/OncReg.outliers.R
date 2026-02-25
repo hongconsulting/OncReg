5bc3f2ca2637de8dc66e3d75211b5e73e7f34ee0
Wei Hong <wei.hong@hongconsulting.ai>
2026-02-25 19:29:24 +1100

    v0.12.0

--- R/OncReg.outliers.R
+++ R/OncReg.outliers.R
@@ -1,9 +1,24 @@
 utils::globalVariables(".OR_nMAD_mat")
-.OR.nMAD_mat <- matrix(NA, nrow = 20, ncol = 1)
-.OR.nMAD_mat[3:20, 1] <- c(
-  10.51, 4.72, 5.41, 4.19, 4.36, 3.86, 3.93,
-  3.66, 3.69, 3.53, 3.55, 3.43, 3.45, 3.36,
-  3.37, 3.31, 3.32, 3.27
+.OR.nMAD_mat <- matrix(NA, nrow = 100, ncol = 2)
+.OR.nMAD_mat[3:100, 1] <- c(
+  10.5, 4.72, 5.41, 4.19, 4.36, 3.86, 3.93, 3.66, 3.69, 3.52, 3.55, 3.43, 3.45,
+  3.36, 3.37, 3.31, 3.32, 3.27, 3.27, 3.23, 3.24, 3.2, 3.21, 3.18, 3.18, 3.16,
+  3.16, 3.14, 3.14, 3.13, 3.13, 3.11, 3.12, 3.1, 3.1, 3.09, 3.09, 3.08, 3.08,
+  3.07, 3.07, 3.07, 3.07, 3.06, 3.06, 3.05, 3.05, 3.05, 3.05, 3.04, 3.04, 3.04,
+  3.04, 3.03, 3.03, 3.03, 3.03, 3.02, 3.02, 3.02, 3.02, 3.01, 3.02, 3.01, 3.01,
+  3.01, 3.01, 3.01, 3.01, 3, 3, 3, 3, 3, 3, 2.99, 3, 2.99, 2.99, 2.99, 2.99,
+  2.99, 2.99, 2.99, 2.99, 2.98, 2.98, 2.98, 2.98, 2.98, 2.98, 2.98, 2.98, 2.98,
+  2.98, 2.98, 2.98, 2.98
+)
+.OR.nMAD_mat[3:100, 2] <- c(
+  21.54, 7.02, 8.05, 5.66, 5.89, 4.98, 5.07, 4.6, 4.65, 4.36, 4.39, 4.2, 4.21,
+  4.08, 4.09, 3.98, 3.99, 3.91, 3.92, 3.85, 3.86, 3.81, 3.81, 3.77, 3.77, 3.73,
+  3.74, 3.7, 3.71, 3.68, 3.68, 3.66, 3.66, 3.64, 3.64, 3.62, 3.62, 3.6, 3.61,
+  3.59, 3.59, 3.58, 3.58, 3.57, 3.57, 3.56, 3.56, 3.55, 3.55, 3.54, 3.54, 3.53,
+  3.53, 3.52, 3.52, 3.51, 3.51, 3.51, 3.51, 3.5, 3.5, 3.5, 3.5, 3.49, 3.49,
+  3.49, 3.49, 3.48, 3.48, 3.48, 3.48, 3.47, 3.47, 3.47, 3.47, 3.46, 3.46, 3.46,
+  3.46, 3.46, 3.46, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.44, 3.44,
+  3.44, 3.44, 3.44, 3.44, 3.44, 3.44, 3.43
 )
 
 #' Small-sample MAD cutoff under normality
@@ -11,12 +26,12 @@ utils::globalVariables(".OR_nMAD_mat")
 #' Returns the number of unscaled median absolute deviations from the median
 #' required to exclude a proportion `p` of observations under normality, used in
 #' the modified Z-score method for non-parametric outlier detection¹. Values
-#' were obtained via Monte Carlo root-finding using 10 million replicates per
-#' iteration and rounded to two decimal places.
-#' @param n Integer sample size, where `3` ≤ `n` ≤ `20`.
-#' @param p Target two-sided exclusion probability. Only `0.05` is currently
-#' implemented.
-#' @return Numeric scalar giving the cutoff in unscaled MAD units.
+#' were pre-calculated via Monte Carlo root-finding using 10 million replicates
+#' per iteration and rounded to two decimal places.
+#' @param n Integer sample size, where `3` ≤ `n` ≤ `100`.
+#' @param p Target two-sided exclusion probability. Only `0.05` and `0.025` are
+#' currently implemented.
+#' @return The cutoff in unscaled MAD units.
 #' @references
 #' 1. Iglewicz, B. and Hoaglin, D.C., 1993. Chapter 3.3 Modified Z-scores.
 #' In: *How to Detect and Handle Outliers*, pp. 11–13. Milwaukee: ASQC Quality
@@ -25,10 +40,33 @@ utils::globalVariables(".OR_nMAD_mat")
 OR.nMAD <- function(n, p = 0.05) {
   if (p == 0.05) {
     col <- 1
+  } else if (p == 0.025) {
+    col <- 2
   } else {
     stop(paste0("[OR.nMAD] not implemented: p =", n))
   }
-  if (n < 3 || n > nrow(.OR_nMAD_mat))
+  if (n < 3 || n > nrow(.OR.nMAD_mat))
     stop(paste0("[OR.nMAD] not implemented: n =", n))
-  return(.OR_nMAD_mat[n, col])
+  return(.OR.nMAD_mat[n, col])
 }
+
+#' Single sample outlier detection using the modified Z-score method
+#'
+#' Flags observations whose absolute deviation from the sample median
+#' exceeds a sample-size–adjusted multiple of the median absolute deviation.
+#' @param x Numeric vector of observations.
+#' @param p Target two-sided exclusion proportion under normality. That is, the
+#' proportion of observations expected to lie beyond the cutoff in a sample of
+#' size `n` from a normal distribution. Default = `0.05`.
+#' @return Logical vector of the same length as `x` where `TRUE` indicates an
+#' outlier.
+#' @export
+OR.outliers <- function(x, p = 0.05) {
+  med <- stats::median(x)
+  mad <- stats::mad(x)
+  return(abs(x - med) > OR.nMAD(n = length(x), p = p) * mad)
+}
+
+# OR.outliers.rlm <- function(y, x, order = 3, p = 0.05) {
+#
+# }
--- R/rlm.R
+++ R/rlm.R
@@ -1,17 +1,20 @@
 #' Robust linear regression using Huber M-estimation
 #'
-#' Fits a linear regression model using Huber M-estimation via iteratively 
+#' Fits a linear regression model using Huber M-estimation¹ via iteratively
 #' reweighted least squares.
 #' @param X numeric design matrix.
 #' @param y numeric response vector.
-#' @param k2 tuning constant for Huber psi. Default = `1.345`.
-#' @param maxit maximum number of iterations. Default = `20`.
+#' @param k2 tuning constant for Huber \ifelse{latex}{\out{$\mathit{\psi}$}}{\ifelse{html}{\out{<i>&psi;</i>}}{*psi*}}-function. Default = `1.345`.
+#' @param maxit maximum number of iterations. Default = `100`.
 #' @param tol convergence tolerance. Default = `0.0001`.
 #' @return
-#' An object of class `"rlm"` and `"lm"`, broadly compatible with objects 
+#' An object of classes `"lm"` and `"rlm"`, broadly compatible with objects
 #' returned by `MASS::rlm()` with minor differences due to rounding.
+#' @references
+#' 1. Huber, P.J., 1973. Robust regression: asymptotics, conjectures and Monte
+#' Carlo. *The Annals of Statistics*, pp. 799–821.
 #' @export
-rlm.Huber <- function(X, y, k2 = 1.345, maxit = 20, tol = 0.0001) {
+rlm.Huber <- function(X, y, k2 = 1.345, maxit = 100, tol = 0.0001) {
   if (qr(X)$rank < ncol(X)) stop("[rlm.Huber] singularity detected")
   init <- stats::lm.fit(X, y)
   coef <- init$coef
@@ -20,19 +23,19 @@ rlm.Huber <- function(X, y, k2 = 1.345, maxit = 20, tol = 0.0001) {
   done <- FALSE
   trace <- NULL
   for (i in 1:maxit) {
-    resid0 <- resid   
+    resid0 <- resid
     scale <- stats::median(abs(resid))/0.6744897501960817054467
     if (scale == 0) {
       done <- TRUE
       break
-    }   
-    w <- pmin(1, 1.345/abs(resid/scale))
+    }
+    w <- pmin(1, k2/abs(resid/scale))
     fit <- stats::lm.wfit(X, y, w, method = "qr")
     coef <- fit$coefficients
     resid <- fit$residuals
     if (!is.null(resid)) {
       delta <- sqrt(sum((resid0 - resid)^2)/max(0.00000000000000000001, sum(resid0^2)))
-    } 
+    }
     trace <- c(trace, delta)
     done <- (delta <= tol)
     if (done) break
@@ -41,15 +44,63 @@ rlm.Huber <- function(X, y, k2 = 1.345, maxit = 20, tol = 0.0001) {
   fitted <- drop(X %*% coef)
   call <- match.call()
   call[[1L]] <- as.name("rlm.Huber")
-  psi.Huber <- function (x, k = 1.345) {
-    return(pmin(1, k/abs(x)))    
+  weight.Huber <- function (x, k = k2) {
+    return(pmin(1, k/abs(x)))
   }
-  output <- list(coefficients = coef, residuals = y - fitted, 
-                 wresid = resid, effects = fit$effects, rank = fit$rank, 
-                 fitted.values = fitted, assign = NULL, qr = fit$qr, 
-                 df.residual = NA, w = w, s = scale, psi = psi.Huber, k2 = k2, 
-                 weights = NULL, conv = trace, 
+  output <- list(coefficients = coef, residuals = y - fitted,
+                 wresid = resid, effects = fit$effects, rank = fit$rank,
+                 fitted.values = fitted, assign = NULL, qr = fit$qr,
+                 df.residual = NA, w = w, s = scale, psi = weight.Huber, k2 = k2,
+                 weights = NULL, conv = trace,
                  converged = done, x = X, call = call)
-  class(output) <- c("rlm", "lm")
+  class(output) <- c("lm", "rlm")
+  return(output)
+}
+
+#' Robust Akaike information criterion for Huber M-estimation
+#'
+#' Computes Ronchetti's robust analogue¹ of the Akaike information criterion²
+#' (AICR) for a linear model fitted by Huber M-estimation³.
+#' @param X numeric design matrix.
+#' @param y numeric response vector.
+#' @param beta numeric vector of regression coefficients from a Huber
+#' M-estimation fit.
+#' @param scale scale estimate from the fit from a Huber M-estimation fit.
+#' @param k2 tuning constant for the Huber \ifelse{latex}{\out{$\mathit{\psi}$}}{\ifelse{html}{\out{<i>&psi;</i>}}{*psi*}}-function. Default = `1.345`.
+#' @return The robust AIC value.
+#' @references
+#' 1. Ronchetti, E., 1985. Robust model selection in regression. *Statistics &
+#' Probability Letters*, 3(1), pp. 21–23.
+#' 2. Akaike, H., 1974. A new look at the statistical model identification.
+#' *IEEE Transactions on Automatic Control*, 19(6), pp. 716–723.
+#' 3. Huber, P.J., 1973. Robust regression: asymptotics, conjectures and Monte
+#' Carlo. *The Annals of Statistics*, pp. 799–821.
+#' @export
+AICR.Huber <- function(y, X, beta, scale, k2 = 1.345) {
+  X <- as.matrix(X)
+  beta <- as.numeric(beta)
+  y <- as.numeric(y)
+  n <- length(y)
+  r <- (y - drop(X %*% beta)) / scale
+  dpsi  <- dscore.Huber(r, k2)
+  psisq <- score.Huber(r, k2)^2
+  X_dpsi  <- X * dpsi
+  X_psisq <- X * psisq
+  J <- crossprod(X_dpsi, X)  / (n * scale^2)
+  K <- crossprod(X_psisq, X) / (n * scale^2)
+  invJ <- solve(J)
+  return(2 * n * log(scale) + 2 * sum(diag(invJ %*% K)))
+}
+
+score.Huber <- function(x, k2) {
+  output <- 2 * k2 * sign(x)
+  i <- abs(x) <= k2
+  output[i] <- 2 * x[i]
+  return(output)
+}
+
+dscore.Huber <- function(x, k2) {
+  output <- numeric(length(x))
+  output[abs(x) <= k2] <- 2
   return(output)
 }
