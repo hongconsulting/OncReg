a7909a1d91d5e5eccd0ec3e834c6c4386ff6892a
Wei Hong <wei.hong@hongconsulting.ai>
2026-03-08 21:43:48 +1100

    v0.13.6

--- R/rlm.R
+++ R/rlm.R
@@ -74,42 +74,41 @@ dscore.Huber <- function(x, k2) {
 rlm.Huber <- function(X, y, k2 = 1.345, maxit = 100, tol = 0.0001) {
   if (qr(X)$rank < ncol(X)) stop("[rlm.Huber] singularity detected")
   init <- stats::lm.fit(X, y)
+  coef <- init$coefficients
+  resid <- init$residuals
   fit <- init
-  coef <- init$coef
-  resid <- init$resid
-  scale <- stats::mad(resid, 0)
-  done <- FALSE
+  w <- rep(1, length(y))
   trace <- NULL
+  done <- FALSE
   for (i in 1:maxit) {
     resid0 <- resid
-    scale <- stats::median(abs(resid))/0.6744897501960817054467
+    scale <- stats::median(abs(resid)) / 0.6744897501960817054467
     if (scale == 0) {
       done <- TRUE
       break
     }
-    w <- pmin(1, k2/abs(resid/scale))
+    w <- pmin(1, k2 / abs(resid / scale))
     fit <- stats::lm.wfit(X, y, w, method = "qr")
     coef <- fit$coefficients
     resid <- fit$residuals
-    if (!is.null(resid)) {
-      delta <- sqrt(sum((resid0 - resid)^2)/max(0.00000000000000000001, sum(resid0^2)))
-    }
+    delta <- sqrt(sum((resid0 - resid)^2) / max(1e-20, sum(resid0^2)))
     trace <- c(trace, delta)
-    done <- (delta <= tol)
-    if (done) break
+    if (delta <= tol) {
+      done <- TRUE
+      break
+    }
   }
   if (!done) stop("[rlm.Huber] non-convergence")
   fitted <- drop(X %*% coef)
+  weight.Huber <- function(x, k = k2) {
+    return(pmin(1, k / abs(x)))
+  }
   call <- match.call()
   call[[1L]] <- as.name("rlm.Huber")
-  weight.Huber <- function (x, k = k2) {
-    return(pmin(1, k/abs(x)))
-  }
-  output <- list(coefficients = coef, residuals = y - fitted,
-                 wresid = resid, effects = fit$effects, rank = fit$rank,
-                 fitted.values = fitted, assign = NULL, qr = fit$qr,
-                 df.residual = NA, w = w, s = scale, psi = weight.Huber, k2 = k2,
-                 weights = NULL, conv = trace,
+  output <- list(coefficients = coef, residuals = y - fitted, wresid = resid,
+                 effects = fit$effects, rank = fit$rank, fitted.values = fitted,
+                 assign = NULL, qr = fit$qr, df.residual = NA, w = w, s = scale,
+                 psi = weight.Huber, k2 = k2, weights = NULL, conv = trace,
                  converged = done, x = X, call = call)
   class(output) <- c("lm", "rlm")
   return(output)
